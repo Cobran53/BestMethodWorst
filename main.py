2b04ac89a7025cb502de003e46f72017d6619411
Nolann Boyere <nolann.boyere@etu.univ-nantes.fr>
2025-03-21 09:30:52 +0100

    oups mauvaise version

--- main.py
+++ main.py
@@ -3,78 +3,85 @@ Criterion = str
 Note = int
 
 import numpy as np
-
 from pyDecision.algorithm import bw_method
+import yaml
+from pprint import pprint
+from tkinter import Tk, filedialog
+
+
 
 class data_compile:
     def __init__(self) -> None:
+        self.isDebug = True
         self.data: dict[Criterion, dict[Country, float|None]]
         # TODO
 
-        self.data = {
-            "Critère 1": {
-                "Pays A": 0.8,
-                "Pays B": 0.6,
-                "Pays C": 0.9, 
-                "Pays sans données": None,
-            },
-            "Critère 2": {
-                "Pays A": 0.7,
-                "Pays B": 0.8,
-                "Pays C": 0.6, 
-                "Pays sans données": None,
-            },
-            "Critère 3": {
-                "Pays A": 0.9,
-                "Pays B": 0.5,
-                "Pays C": 0.7, 
-                "Pays sans données": None,
-            }, 
-            "Critère sans données": {
+        with open("data.yaml", "r") as file:
+            self.data = yaml.safe_load(file)
 
-            }
-        }
-        """self.data = pd.DataFrame({
-            "Critère 1": {
-            "Pays A": 0.8,
-            "Pays B": 0.6,
-            "Pays C": 0.9, 
-            "Pays sans données": None,
-            },
-            "Critère 2": {
-            "Pays A": 0.7,
-            "Pays B": 0.8,
-            "Pays C": 0.6, 
-            "Pays sans données": None,
-            },
-            "Critère 3": {
-            "Pays A": 0.9,
-            "Pays B": 0.5,
-            "Pays C": 0.7, 
-            "Pays sans données": None,
-            }
-        }).T*/"""
-        self.generateMostImportantCriteriaWeights()
-        self.generateLeastImportantCriteriaWeights()
+        if self.isDebug:
+            pprint(self.data)
+            print()
+        
+        self.fillEmptyLines()
+
+        if self.isDebug:
+            pprint(self.data)
+            print()
+        
+        if self.askUserIfImport():
+            self.importMICAndLIC()
+        else:
+            self.generateMostImportantCriteriaWeights()
+            self.generateLeastImportantCriteriaWeights()
+            self.saveMICAndLIC()
+
         self.generateActualWeights()
         self.calculateScoresByCountries()
 
         print(self.countriesScores)
 
+    def askUserIfImport(self) -> bool: 
+        while True:
+            choice = input("Voulez-vous importer les poids des critères à partir d'un fichier YAML ? (oui/non) : ").strip().lower()
+            if choice in ["oui", "o", "y", "yes"]:
+                return True
+            elif choice in ['non', "no", "n"]:
+                return False
+            print("Réponse incorrecte.")
+
+    def importMICAndLIC(self):
+        root = Tk()
+        root.withdraw()  # Masquer la fenêtre principale
+        root.update()  # S'assurer que la fenêtre est correctement initialisée
+        root.attributes('-topmost', True)
+        file_path = filedialog.askopenfilename(title="Sélectionnez le fichier des poids des critères", filetypes=[("Fichiers YAML", "*.yaml")])
+        
+        if file_path:
+            with open(file_path, "r") as file:
+                weights = yaml.safe_load(file)
+                self.mostImportantCriteriaNotes = weights["most_important_criteria"]
+                self.leastImportantCriteriaNotes = weights["least_important_criteria"]
+        else:
+            print("Aucun fichier sélectionné. Fermeture du programme.")
+            exit()
+
+        root.destroy()          
+
     def fillEmptyLines(self):
         
-        # Rejeter les critères ou on a pas assez de donneés
+        # Rejeter les critères où on a pas assez de donneés
         REJECT_CRITERIA_RATE = 0.5
         
-        criteria_to_remove = []
-        for criteria, countries in self.data.items():
-            filled_country_count = sum(1 for value in countries.values() if value is not None)
-            if filled_country_count / len(countries) < REJECT_CRITERIA_RATE:
-                criteria_to_remove.append(criteria)
+        criteria_to_delete = []
+        for criteria in self.data:
+            country_data_count = sum(1 for country in self.data[criteria] if self.data[criteria][country])
+            if country_data_count/len(self.data[criteria].keys()) < REJECT_CRITERIA_RATE:
+                criteria_to_delete.append(criteria)
 
-        for criteria in criteria_to_remove:
+        for criteria in criteria_to_delete:
+            print(f"Critère \"{criteria}\" supprimé : pas assez de données \n")
             del self.data[criteria]
-
 
         # Rejeter les pays où on a pas assez de données
         REJECT_COUNTRY_RATE = 0.5
@@ -83,15 +90,22 @@ class data_compile:
 
         countries_to_remove = []
         for country in self.data[first_criteria].keys():
-            filled_criteria_count = sum(1 for criteria in self.data if self.data[criteria][country] is not None)
-            if filled_criteria_count / len(self.data) < REJECT_COUNTRY_RATE:
+            criteria_data_count = sum(1 for criteria in self.data if self.data[criteria][country] is not None)
+            if criteria_data_count / len(self.data) < REJECT_COUNTRY_RATE:
                 countries_to_remove.append(country)
 
         for country in countries_to_remove:
+            print(f"Pays \"{country}\" supprimé : pas assez de données \n")
             for criteria in self.data:
                 del self.data[criteria][country]
-        
-        
+
+        # Remplacer par la moyenne dans les critères ou on a assez de données mais pas toutes 
+        for criteria in self.data:
+            values = [self.data[criteria][country] for country in self.data[criteria] if self.data[criteria][country] is not None]
+            mean_value = np.mean(values) if values else 0
+            for country in self.data[criteria]:
+                if self.data[criteria][country] is None:
+                    self.data[criteria][country] = mean_value
 
     def generateMostImportantCriteriaWeights(self) -> dict[Criterion, Note]:
         self.mostImportantCriteria: Criterion # nous déterminons qu'un des critères est le plus important
@@ -120,7 +134,7 @@ class data_compile:
 
         print("Évaluez l'importance de chaque critère par rapport au critère le plus important, de 1 à 9, avec \n" 
               "• 1 : le meilleur critère est aussi important que ce critère, \n"
-              "• 9 : le meilleur critère est extremement plus important que ce critère)\n")
+              "• 9 : le meilleur critère est extremement plus important que ce critère.\n")
         for crit in criteria:
             if crit != self.mostImportantCriteria:
                 while True:
@@ -134,10 +148,6 @@ class data_compile:
                     except ValueError:
                         print("Entrée invalide. Veuillez entrer un numéro valide.")
                 self.mostImportantCriteriaNotes[crit] = note
-
-        with open("most_important_criteria_weights.txt", "w") as file:
-            for crit, note in self.mostImportantCriteriaNotes.items():
-                file.write(f"{crit}: {note}\n")
 
     def generateLeastImportantCriteriaWeights(self) -> dict[Criterion, Note]:
         self.leastImportantCriteria: Criterion # idem pour le pire
@@ -164,7 +174,7 @@ class data_compile:
 
         print("Évaluez l'importance de chaque critère par rapport au critère le moins important, de 1 à 9, avec \n" 
               "• 1 : le pire critère est aussi important que ce critère, \n"
-              "• 9 : le pire critère est extrêmement moins important que ce critère)\n")
+              "• 9 : le pire critère est extrêmement moins important que ce critère.\n")
         for crit in criteria:
             if crit != self.leastImportantCriteria:
                 while True:
@@ -179,9 +189,13 @@ class data_compile:
                         print("Entrée invalide. Veuillez entrer un numéro valide.")
                 self.leastImportantCriteriaNotes[crit] = note
 
-        with open("least_important_criteria_weights.txt", "w") as file:
-            for crit, note in self.leastImportantCriteriaNotes.items():
-                file.write(f"{crit}: {note}\n")
+
+    def saveMICAndLIC(self):
+        with open("criteria_weights.yaml", "w") as file:
+            yaml.dump({
+            "most_important_criteria": self.mostImportantCriteriaNotes,
+            "least_important_criteria": self.leastImportantCriteriaNotes
+            }, file)
 
     def generateActualWeights(self) -> dict[Criterion, float]:
         self.actualWeights: dict[Criterion, float] # utiliser l'exemple BWM pour générer les poids pour chaque critère
@@ -191,7 +205,7 @@ class data_compile:
         mic = np.array([self.mostImportantCriteriaNotes[crit] for crit in criteria])
         lic = np.array([self.leastImportantCriteriaNotes[crit] for crit in criteria])
         
-        weights = bw_method(mic, lic, eps_penalty=1, verbose=True)
+        weights = bw_method(mic, lic, eps_penalty=1, verbose=self.isDebug)
         
         self.actualWeights = {crit: weights[i] for i, crit in enumerate(criteria)}
 
@@ -206,3 +220,16 @@ class data_compile:
         return self.countriesScores
     
 A = data_compile()
+
+
+# Faire une précision après avoir trouver le pays
+# Ah, j'ai trouvé la france, donc maintenant je regarde la région française
+# On fait ça que pour la france, mais au moins c'est développé
+
+
+# faire en fonction de le localisation
+
+
+# sauvegarde en .yaml
+
+# 
